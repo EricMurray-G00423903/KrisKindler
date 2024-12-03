f3bd9266a6d9069c6a67a90c273e366ecfa242f2
EricMurray-G00423903 <g00423903@atu.ie>
2024-12-03 21:33:46 +0000

    added routing for new joingroup

--- kriskindler/src/App.js
+++ kriskindler/src/App.js
@@ -4,6 +4,7 @@ import Home from './Pages/Home';  // Import the Home component
 import Create from './Pages/Create';  // Import the Create component
 import View from "./Pages/View";  // Import the View component
 import Wishlist from "./Pages/Wishlist";  // Import the Wishlist component
+import JoinGroup from "./Pages/JoinGroup";  // Import the JoinGroup component
 
 // App component handles the routing for the application
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <Route path="/create" element={<Create />} />
         <Route path="/view" element={<View />} />
         <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/join-group" element={<JoinGroup />} />
       </Routes>
     </Router>
   );
